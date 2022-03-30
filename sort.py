2960d91de00aff4936790f7ab4ed5a28f8216eba
Hannah Okwelum <hokwelum@wikimedia.org>
2022-03-30 08:35:06 +0100

    Add unittest and also modified find function

--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+from os.path import exists
 """The find function takes in two parameters:
  The word path and the paragraph path: and returns the words found in DICT Type
   and the number of occurrence from the most occurred to least found"""
@@ -5,6 +6,7 @@
 
 def find(words_path, paragraph_path):
     """Opens the files  """
+
     with open(words_path, "r", encoding='utf8') as word_lists:
         word_content = word_lists.read().lower().split(", ")
     with open(paragraph_path, "r", encoding='utf8') as paragraphs:
@@ -17,8 +19,24 @@ def find(words_path, paragraph_path):
         if word in paragraphs_content:
             found[word] = paragraphs_content.count(word)
 
-    print(found.items())
-    print(dict(sorted(found.items(), key=lambda item: item[1], reverse=True)))
+    # print(found.items())
+    result = dict(sorted(found.items(), key=lambda item: item[1], reverse=True))
+
+    print(result)
+
+    return result
 
 
-find("words.txt", "paragraph.txt")
+input_word_path = input("Please type in your word path\n")
+input_paragraph_path = input("Please type in your paragraph path\n")
+
+
+def check_file(words_path, paragraph_path):
+
+    if exists(words_path) and exists(paragraph_path):
+        find(words_path, paragraph_path)
+    else:
+        return "Invalid file"
+
+
+check_file(input_word_path, input_paragraph_path)
